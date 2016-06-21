047a6f85c8006f149daead9606ca0500ffcaf6c7
Michal Czaplinski <michal.czaplinski@gxp-brain.com>
2016-06-21 17:08:28 +0200

    finished exercise 2 from homework 1, quick and dirty

--- Homework1-hanoi.hs
+++ Homework1-hanoi.hs
@@ -3,9 +3,10 @@ type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi n a b c
+  | n == 0 = []
+  | n == 1 = [(a, c)]
   | n == 2 = [(a, b), (a, c), (b, c)]
-  | n > 2 = hanoi (n-1) a b c ++ [(a, b)] ++ 
-
+  | n > 2 = hanoi (n-1) a c b ++ [(a, c)] ++ hanoi (n-1) c b a
 
 main :: IO()
 main = do
