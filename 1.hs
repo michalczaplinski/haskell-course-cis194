21063034d93508e1e7ad32ddf44010f3ebc01e7d
Michal Czaplinski <michal.czaplinski@gxp-brain.com>
2016-06-21 14:36:59 +0200

    1 exercise ready

--- 1.hs
+++
@@ -1,9 +0,0 @@
-toDigits :: Integer -> [Integer]
-toDigits 0 = []
-toDigits x = toDigits (div x 10) ++ [mod x 10]
-
-toDigitsRev :: Integer -> [Integer]
-toDigitsRev x = reverse (toDigits x)
-
-doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther x = doubleEveryOther (init (init x)) ++ [(last (init x)) * 2] ++ [last x]
